343132b07381198b856bc3e6ba9737de8c71a085
Devdutt Sharma <64033478+devdutt13@users.noreply.github.com>
2023-11-17 20:00:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -75,4 +75,5 @@ class VideoTransformer(VideoTransformerBase):
 
 st.title('ISL Detector')
 
-webrtc_streamer(key="example", video_processor_factory=VideoTransformer)
+webrtc_streamer(key="example", video_processor_factory=VideoTransformer,rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
+
