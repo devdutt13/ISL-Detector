211b45da0b48bad36233a46db2084132986ec10f
Devdutt Sharma <64033478+devdutt13@users.noreply.github.com>
2023-11-17 20:28:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,11 @@ mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
 @st.cache(hash_funcs={"MyUnhashableClass": lambda _: None}
+	  
 @st.cache
 def load_model():
-	  return keras.models.load_model("sign_recognition_model.h5")
+	return keras.models.load_model("sign_recognition_model.h5")
+	
 model = load_model()
 labels_dict = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8, 'J': 9,
                'K': 10, 'L': 11, 'M': 12, 'N': 13, 'O': 14, 'P': 15, 'Q': 16, 'R': 17, 'S': 18,
